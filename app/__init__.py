dce366e6d982a286b43b84f3c8c83d295554dfa8
melisajuma <Melisaakinyi95@gmail.com>
2019-08-19 16:25:19 +0300

    Initializing flask extensions

--- app/__init__.py
+++ app/__init__.py
@@ -20,13 +20,10 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    
+    app.config.from_object(config_options[config_name])
 
     # Initializing flask extensions
-    bootstrap.init_app(app)
-    db.init_app(app)
-    login_manager.init_app(app)
-    mail.init_app(app)
+   
 
     # Registering the blueprint
     from .main import main as main_blueprint
