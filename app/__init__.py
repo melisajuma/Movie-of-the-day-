867c9456f1eade7d3ffcf61e05679b7a4da3cf1a
melisajuma <Melisaakinyi95@gmail.com>
2019-08-19 16:27:00 +0300

    authentication

--- app/__init__.py
+++ app/__init__.py
@@ -33,10 +33,10 @@ def create_app(config_name):
     app.register_blueprint(main_blueprint)
 
     # setting config
-   
+    from .request import configure_request
+    configure_request(app)
 
     # auth
-    from .auth import auth as auth_blueprint
-    app.register_blueprint(auth_blueprint, url_prefix='/authenticate')
+   
 
     return app
