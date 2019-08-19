6134cc8f973fd7afe0f0211039e88ea3a4756123
melisajuma <Melisaakinyi95@gmail.com>
2019-08-19 16:24:42 +0300

    Creating the app configurations

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import *
 from flask_sqlalchemy import SQLAlchemy
-
+from flask_login import LoginManager
+from flask_mail import Mail, Message
 
 
 db=SQLAlchemy()
@@ -19,7 +20,7 @@ def create_app(config_name):
     app = Flask(__name__)
 
     # Creating the app configurations
-    app.config.from_object(config_options[config_name])
+    
 
     # Initializing flask extensions
     bootstrap.init_app(app)
